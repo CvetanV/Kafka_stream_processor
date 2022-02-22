92620fed7d24fb315d2be74dc91a5615628b15bc
Cvetan Veljanovski <cvetan.veljanovski@ext.illimity.com>
2022-02-22 12:04:34 +0100

    Adding workflow, make and requirements files

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -4,16 +4,17 @@ from kafka import KafkaConsumer
 from pymongo import MongoClient
 
 # generating the Kafka Consumer
-data_consumer = KafkaConsumer('weather_data1', # tag of the sent data
-                              bootstrap_servers = ['localhost : 9092'],
-                              auto_offset_reset = 'earliest',
-                              enable_auto_commit = True,
-                              group_id = 'my-group',
-                              value_deserializer = lambda x : loads(x.decode('utf-8'))
-                              )
+data_consumer = KafkaConsumer(
+    "weather_data1",  # tag of the sent data
+    bootstrap_servers=["localhost : 9092"],
+    auto_offset_reset="earliest",
+    enable_auto_commit=True,
+    group_id="my-group",
+    value_deserializer=lambda x: loads(x.decode("utf-8")),
+)
 
 # Connect to mongo db and colection
-mongo_db_client = MongoClient('localhost:27017')
+mongo_db_client = MongoClient("localhost:27017")
 mongo_collection = mongo_db_client.kafka_db.kafka_weather_data
 
 # iterate through the data sent by the producer
--- kafka_producer.py
+++ kafka_producer.py
@@ -1,27 +1,31 @@
 # importing the required libraries
-import pandas as pd
 from time import sleep
 from json import dumps
 from kafka import KafkaProducer
+import pandas as pd
+
 
 # initializing the Kafka producer
-data_producer = KafkaProducer(bootstrap_servers = ['localhost:9092'],
-                              value_serializer = lambda x:dumps(x).encode('utf-8')
-                              )
+data_producer = KafkaProducer(
+    bootstrap_servers=["localhost:9092"],
+    value_serializer=lambda x: dumps(x).encode("utf-8"),
+)
 
 # Read data from the csv with Pandas
-df = pd.read_csv("dataset.csv", sep=';')
+dataFrame = pd.read_csv("dataset.csv", sep=";")
 
 # Transform dataframe to a dictionary
-def to_dictionary(df):
-    df.set_index("ID", drop=True, inplace=True)
-    dictionary = df.to_dict(orient="index")
+def to_dictionary(data):
+    data.set_index("ID", drop=True, inplace=True)
+    dictionary = data.to_dict(orient="index")
     return dictionary
-dict_data = to_dictionary(df)
+
+
+dict_data = to_dictionary(dataFrame)
 
 # Read dictionary items
 for item in dict_data.items():
-    data = {'Weather Data': item}
-    data_producer.send('weather_data1', value=data)
+    data = {"Weather Data": item}
+    data_producer.send("weather_data1", value=data)
     print("Sent", data)
-    #sleep(1)
+    sleep(1)
